ef02ce2eb985e9b7514c2a7c137aaacfa034e0a1
dotrong-code <dotrong159357@gmail.com>
2025-01-02 19:56:03 +0700

    part 4

---
+++ Seed.BackEnd/Seed.Application/DTOs/Common/CurrentUserObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seed.Application.DTOs.Common
+{
+    public class CurrentUserObject
+    {
+        public Guid UserId { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public int Role { get; set; }//1 admin, 2 manager,3 veterinarian, 4 staff, 5 customer
+        public string RoleName { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ Seed.BackEnd/Seed.Host/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using Microsoft.AspNetCore.Mvc;
+using Seed.Application.Common.Result;
+using Seed.Application.DTOs.User.Login;
+using Seed.Application.Interface.IService;
+
+namespace Seed.Host.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("sign-in")]
+        public async Task<IResult> SignInForUser(LoginRequest loginRequest)
+        {
+            Result result = await _authService.SignIn(loginRequest);
+            return result.IsSuccess
+                ? ResultExtensions.ToSuccessDetails(result, "Login successfully")
+                : ResultExtensions.ToProblemDetails(result);
+        }
+    }
+}
