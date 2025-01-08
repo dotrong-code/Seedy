fefdf9c3761e4f9dfbbe5e3201ea466d07f9c067
dotrong-code <dotrong159357@gmail.com>
2025-01-09 00:33:04 +0700

    Product+Cate

---
+++ Seed.BackEnd/Seed.Application/Common/Validator/ProductVali/UpdateProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seed.Application.Common.Validator.Abstract;
+using Seed.Infrastructure.Common;
+using Seed.Infrastructure.DTOs.Product.Update;
+
+namespace Seed.Application.Common.Validator.ProductVali
+{
+    public class UpdateProductValidator : ProductValidator<UpdateProductRequest>
+    {
+        public UpdateProductValidator(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            AddProductNameRules(request => request.Name, isRequired: false);
+            AddProductPriceRules(priceExpression => priceExpression.Price, isRequired: false);
+            AddProductDescriptionRules(descriptionExpression => descriptionExpression.Description, isRequired: false);
+        }
+    }
+}
---
+++ Seed.BackEnd/Seed.Infrastructure/DTOs/Product/Update/UpdateProductRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seed.Infrastructure.DTOs.Product.Create;
+
+namespace Seed.Infrastructure.DTOs.Product.Update
+{
+    public class UpdateProductRequest
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public int? StockQuantity { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
