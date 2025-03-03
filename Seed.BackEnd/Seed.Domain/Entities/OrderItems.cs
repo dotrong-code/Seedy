4ce9dc146b4f0f34d198f79005b552c611f901ba
dotrong-code <dotrong159357@gmail.com>
2025-03-03 17:17:02 +0700

    cmittt

--- Seed.BackEnd/Seed.Domain/Entities/OrderItems.cs
+++ Seed.BackEnd/Seed.Domain/Entities/OrderItems.cs
@@ -9,7 +9,7 @@ namespace Seed.Domain.Entities
     public class OrderItem : BaseEntity
     {
         public Guid OrderId { get; set; } // Foreign key to Order       
-        public Guid ProductId { get; set; } // Optional foreign key to Product
+        public Guid? ProductId { get; set; } // Optional foreign key to Product
         public int Quantity { get; set; }
         public decimal Price { get; set; }
 
